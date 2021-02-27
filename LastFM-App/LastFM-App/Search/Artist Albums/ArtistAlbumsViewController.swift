3c2ba4fe9ca8b86be5880cd8fe7b3fd97eab83f0
Erich Flock <flock91@gmail.com>
2021-02-27 17:12:14 +0100

    Add collection view to ArtistAlbumsVC

--- LastFM-App/LastFM-App/Search/Artist Albums/ArtistAlbumsViewController.swift
+++ LastFM-App/LastFM-App/Search/Artist Albums/ArtistAlbumsViewController.swift
@@ -11,6 +11,8 @@ class ArtistAlbumsViewController: UIViewController {
     
     let artistName: String
     
+    private var collectionView: UICollectionView?
+    
     init(artistName: String) {
         self.artistName = artistName
         super.init(nibName: nil, bundle: nil)
@@ -22,6 +24,48 @@ class ArtistAlbumsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        setupTitle()
+        setupCollectionView()
+    }
+    
+    private func setupTitle() {
         title = "Albums"
     }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = .init(width: 96, height: 84)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else { return }
+        collectionView.backgroundColor = .systemBackground
+        collectionView.dataSource = self
+        collectionView.register(ArtistAlbumCollectionViewCell.self, forCellWithReuseIdentifier: "ArtistAlbumCollectionViewCell")
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8),
+        ])
+    }
+}
+
+extension ArtistAlbumsViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ArtistAlbumCollectionViewCell", for: indexPath) as? ArtistAlbumCollectionViewCell
+        cell?.viewModel = .init(imageUrlString: "", title: "Top Album")
+        return cell ?? UICollectionViewCell()
+    }
+    
 }
