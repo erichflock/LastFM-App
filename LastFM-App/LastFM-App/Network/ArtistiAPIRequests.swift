0f5cd77e6a37e19a7c41520f1629878b7bf3964d
Erich Flock <flock91@gmail.com>
2021-02-28 11:26:10 +0100

    Move CaseIterableDefault to its own file
    - Prootcol was created to avoid crash when custom type value is not in response.

--- LastFM-App/LastFM-App/Network/ArtistiAPIRequests.swift
+++ LastFM-App/LastFM-App/Network/ArtistiAPIRequests.swift
@@ -68,19 +68,3 @@ struct ArtistSearchResultRoot: Codable {
         }
     }
 }
-
-protocol CaseIterableDefault: Codable & CaseIterable & RawRepresentable where RawValue: Codable {
-    static var defaultValue: Self { get }
-}
-
-extension CaseIterableDefault {
-    
-    init(from decoder: Decoder) throws {
-        let rawValue = try decoder.singleValueContainer().decode(RawValue.self)
-        if let initValue = Self(rawValue: rawValue) {
-            self = initValue
-        } else {
-            self = Self.defaultValue
-        }
-    }
-}
