a45fc249898b212ce347ea37d32e201c7246e98e
Erich Flock <flock91@gmail.com>
2021-02-28 17:49:11 +0100

    Add missing file from previous commit

--- LastFM-App/LastFM-App/Model/Album.swift
+++ LastFM-App/LastFM-App/Model/Album.swift
@@ -9,4 +9,9 @@ struct Album {
     var name: String
     var imageURLString: String
     var artistName: String
+    var tracks: [Track]?
+    
+    struct Track {
+        var name: String
+    }
 }
